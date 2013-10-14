212f3ccc9d0844f63a3a3cacc23b80732e4fe674
xbrauli0x <xka0zx@msn.com>
2013-10-14 10:01:28 -0500

    Determinar Direccionamiento - P03
    Se determina el modo de direccionamiento del codop mediante su operando.

--- Principal.java
+++ Principal.java
@@ -42,6 +42,22 @@ public class Principal extends JFrame {
 	public Vector<JTextField> vtxt=new Vector<JTextField>();
 	public Vector<Tabop> vtab=new Vector<Tabop>();
 	
+	public Vector<Tabop> inh=new Vector<Tabop>();
+	public Vector<Tabop> imm8=new Vector<Tabop>();
+	public Vector<Tabop> imm16=new Vector<Tabop>();
+	public Vector<Tabop> dir=new Vector<Tabop>();
+	public Vector<Tabop> ext=new Vector<Tabop>();
+	public Vector<Tabop> idx=new Vector<Tabop>();
+	public Vector<Tabop> idx1=new Vector<Tabop>();
+	public Vector<Tabop> idx2=new Vector<Tabop>();
+	public Vector<Tabop> idxx2=new Vector<Tabop>();
+	public Vector<Tabop> idxpp=new Vector<Tabop>();
+	public Vector<Tabop> idxac=new Vector<Tabop>();
+	public Vector<Tabop> didx=new Vector<Tabop>();
+	public Vector<Tabop> rel8=new Vector<Tabop>();
+	public Vector<Tabop> rel9=new Vector<Tabop>();
+	public Vector<Tabop> rel16=new Vector<Tabop>();
+	
 	int contlinea=0, banend=0;
 	String cad="";
 	private JScrollPane scrollPane;
@@ -50,6 +66,7 @@ public class Principal extends JFrame {
 	private JToolBar toolBar;
 	private JPanel panel_3;
 	private JPanel panel_1;
+	private JLabel lblModo;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -65,6 +82,10 @@ public class Principal extends JFrame {
 	}
 
 	public Principal() {
+
+		String num="ffff";
+		int comp=(int) Math.pow(16, num.length()) -1;
+		System.out.println(comp);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 525, 304);
 		contentPane = new JPanel();
@@ -111,7 +132,7 @@ public class Principal extends JFrame {
 		
 		panel_1 = new JPanel();
 		panel.add(panel_1, BorderLayout.NORTH);
-		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 35, 5));
+		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 45, 5));
 		
 		JLabel lblLinea = new JLabel("L\u00EDnea");
 		panel_1.add(lblLinea);
@@ -119,11 +140,14 @@ public class Principal extends JFrame {
 		JLabel lblEtiq = new JLabel("Etiqueta");
 		panel_1.add(lblEtiq);
 		
-		JLabel lblCodop = new JLabel("C\u00F3digo de Operaci\u00F3n");
+		JLabel lblCodop = new JLabel("CodOP");
 		panel_1.add(lblCodop);
 		
 		JLabel lblOperador = new JLabel("Operador");
 		panel_1.add(lblOperador);
+		
+		lblModo = new JLabel("Modo");
+		panel_1.add(lblModo);
 		
 		try
 		{
@@ -145,11 +169,48 @@ public class Principal extends JFrame {
 					t.byxc=Integer.parseInt(arr[3]);
 					t.sby=Integer.parseInt(arr[4]);
 					vtab.add(t);
+					
+					if(t.modo.equalsIgnoreCase("INH"))
+						inh.add(t);
+					else
+						if(t.modo.equalsIgnoreCase("IMM8"))
+							imm8.add(t);
+						else
+							if(t.modo.equalsIgnoreCase("IMM16"))
+								imm16.add(t);
+							else
+								if(t.modo.equalsIgnoreCase("DIR"))
+									dir.add(t);
+								else
+									if(t.modo.equalsIgnoreCase("EXT"))
+										ext.add(t);
+									else
+										if(t.modo.equalsIgnoreCase("IDX"))
+											idx.add(t);
+										else
+											if(t.modo.equalsIgnoreCase("IDX1"))
+												idx1.add(t);
+											else
+												if(t.modo.equalsIgnoreCase("IDX2"))
+													idx2.add(t);
+												else
+													if(t.modo.equalsIgnoreCase("[IDX2]"))
+														idxx2.add(t);
+													else
+														if(t.modo.equalsIgnoreCase("[D,IDX]"))
+															didx.add(t);
+														else
+															if(t.modo.equalsIgnoreCase("REL8"))
+																rel8.add(t);
+															else
+																if(t.modo.equalsIgnoreCase("REL9"))
+																	rel9.add(t);
+																else
+																	if(t.modo.equalsIgnoreCase("REL16"))
+																		rel16.add(t);
 				}
 			}
 			
-			for(Tabop tx: vtab)
-				System.out.println(tx.ins+"\t"+tx.modo+"\t"+tx.codmaq+"\t"+tx.byc+"\t"+tx.byxc+"\t"+tx.sby);
 			fr.close();
 			br.close();
 		}
@@ -198,15 +259,11 @@ public class Principal extends JFrame {
 							{
 								String str[]=com.split(";",2);
 								if(str.length>1)
-								{
 									tkn.add(str[0]);
-										
-								}
 								else
 									tkn.add(com);
 							}
 						}
-						
 						if(tkn.size()>0)
 								Analizar(tkn.size(),tkn);
 						
@@ -215,17 +272,12 @@ public class Principal extends JFrame {
 						    	break;
 						contlinea++;
 					}
+					
 					fr.close();
 					br.close();
 					
 					if(!ValidarEnd())
-					   Errores(4,0,"");
-					else
-					{
-						if(vfinal.lastElement().etq.equalsIgnoreCase("NULL"))
-							vfinal.lastElement().etq="NULL";
-						vfinal.lastElement().oper="NULL";
-					}
+						   Errores(4,0,"");
 					
 					if(vfinal.size()>0)
 					{
@@ -299,7 +351,7 @@ public class Principal extends JFrame {
 	{
 		int banm=0,banopr=0;
 		
-		Linea l=new Linea(elista,contlinea);
+		Linea l=new Linea(elista,contlinea,vtab);
 		switch(opc)
 		{
 			case 1:
@@ -307,6 +359,21 @@ public class Principal extends JFrame {
 				{
 					if(l.AnalizarCodop(tkn.get(0)))
 					{
+						if(tkn.get(0).equalsIgnoreCase("ORG"))
+						{
+							Errores(9,0,"");
+							break;
+						}
+						else
+						{
+							if(tkn.get(0).equalsIgnoreCase("END"))
+							{
+								l.codop=tkn.get(0);
+								l.linea=Integer.toString(contlinea);
+								vfinal.add(l);
+								break;
+							}
+						}
 						
 						for(Tabop tb: vtab)
 						{
@@ -317,23 +384,16 @@ public class Principal extends JFrame {
 									banopr=1;
 									break;
 								}
-								
-								if(banm==0)
+								else
 								{
-									
 									l.codop=tkn.get(0);
 									l.linea=Integer.toString(contlinea);
 									l.modos=tb.modo;
+									vfinal.add(l);
 									banm=1;
-								}
-								else
-									l.modos+=", "+tb.modo;
-							}
-							else
-								if(!tb.ins.equalsIgnoreCase(tkn.get(0)) && banm==1)
 									break;
-							
-							
+								}
+							}
 						}
 						
 						if(banopr==1)
@@ -342,11 +402,7 @@ public class Principal extends JFrame {
 							break;
 						}
 						
-						if(banm==1)
-						{
-							vfinal.add(l);
-						}
-						else
+						if(banm==0)
 							Errores(6,0,tkn.get(0));
 					}
 				}
@@ -359,48 +415,74 @@ public class Principal extends JFrame {
 				{
 					if(l.AnalizarCodop(tkn.get(0)))
 					{
-						for(Tabop tb: vtab)
+						if(tkn.get(0).equalsIgnoreCase("ORG"))
 						{
-							if(tb.ins.equalsIgnoreCase(tkn.get(0)))
+							if(l.RegularizarBase(tkn.get(1)))
 							{
-								if(tb.byxc==0)
-									banopr=1;
-								
-								if(banm==0)
-								{
-									l.codop=tkn.get(0);
-									l.oper=tkn.get(1);
-									l.linea=Integer.toString(contlinea);
-									l.modos=tb.modo;
-									banm=1;
-								}
-								else
-									l.modos+=", "+tb.modo;
-								
+								l.codop=tkn.get(0);
+								l.oper=tkn.get(1);
+								l.linea=Integer.toString(contlinea);
+								vfinal.add(l);
+								banm=1;
+							}
+						}
+						else
+						{
+							if(tkn.get(0).equalsIgnoreCase("END"))
+							{
+								l.codop=tkn.get(0);
+								l.linea=Integer.toString(contlinea);
+								vfinal.add(l);
+								break;
 							}
 							else
-								if(!tb.ins.equalsIgnoreCase(tkn.get(0)) && banm==1)
-									break;
-							
-						}
-						if(banopr==1)
-						{
-							Errores(7,0,tkn.get(0));
-							break;
+							{
+								for(Tabop tb: vtab)
+								{
+									if(tb.ins.equalsIgnoreCase(tkn.get(0)))
+									{
+										if(tb.byxc==0)
+											Errores(7,0,tkn.get(0));
+										
+										if(l.AnalizarOper(tkn.get(1),tkn.get(0)))
+										{
+											l.codop=tkn.get(0);
+											l.oper=tkn.get(1);
+											l.linea=Integer.toString(contlinea);
+											vfinal.add(l);
+										}
+										banm=1;
+										break;
+									}
+								}
+							}
 						}
 						
-						
-						if(banm==1)
-							vfinal.add(l);
-						else
-							Errores(6,0,tkn.get(0));
+						if(banm==0)
+						  Errores(6,0,tkn.get(0));
 					}
-					
 				}
 				else
 				{
 					if(l.AnalizarEtq(tkn.get(0)) && l.AnalizarCodop(tkn.get(1)))
 					{
+						if(tkn.get(1).equalsIgnoreCase("ORG"))
+						{
+							Errores(9,0,"");
+							break;
+						}
+						else
+						{
+							if(tkn.get(1).equalsIgnoreCase("END"))
+							{
+								l.codop=tkn.get(1);
+								l.etq=tkn.get(0);
+								l.linea=Integer.toString(contlinea);
+								vfinal.add(l);
+								break;
+							}
+						}
+						
 						for(Tabop tb: vtab)
 						{
 							if(tb.ins.equalsIgnoreCase(tkn.get(1)))
@@ -410,21 +492,19 @@ public class Principal extends JFrame {
 									banopr=1;
 									break;
 								}
+								
 								if(banm==0)
 								{
 									l.codop=tkn.get(1);
 									l.etq=tkn.get(0);
+									if(tb.byxc==0)
+										l.modos=tb.modo;
 									l.linea=Integer.toString(contlinea);
-									l.modos=tb.modo;
 									banm=1;
 								}
-								else
-									l.modos+=", "+tb.modo;
 								
+								break;
 							}
-							else
-								if(!tb.ins.equalsIgnoreCase(tkn.get(1)) && banm==1)
-									break;
 								
 						}
 						
@@ -447,29 +527,49 @@ public class Principal extends JFrame {
 				{
 					if(l.AnalizarEtq(tkn.get(0)) && l.AnalizarCodop(tkn.get(1)))
 					{
-						for(Tabop tb: vtab)
+						if(tkn.get(1).equalsIgnoreCase("ORG"))
 						{
-							if(tb.ins.equals(tkn.get(1)))
+							if(l.RegularizarBase(tkn.get(2)))
 							{
-								if(tb.byxc==0)
-									banopr=1;
-								
-								if(banm==0)
-								{
-									l.codop=tkn.get(1);
-									l.etq=tkn.get(0);
-									l.oper=tkn.get(2);
-									l.linea=Integer.toString(contlinea);
-									l.modos=tb.modo;
-									banm=1;
-								}
-								else
-									l.modos+=", "+tb.modo;
-								
+								l.etq=tkn.get(0);
+								l.codop=tkn.get(1);
+								l.oper=tkn.get(2);
+								l.linea=Integer.toString(contlinea);
+								vfinal.add(l);
+								banm=1;
+								vfinal.add(l);
 							}
-							else
-								if(!tb.ins.equalsIgnoreCase(tkn.get(1)) && banm==1)
+						}
+						else
+						{
+							if(tkn.get(1).equalsIgnoreCase("END"))
+							{
+								Errores(6,0,tkn.get(1));
+								break;
+							}
+							
+							for(Tabop tb: vtab)
+							{
+								if(tb.ins.equalsIgnoreCase(tkn.get(1)))
+								{
+									if(tb.byxc==0)
+									{
+										banopr=1;
+										break;
+									}
+									
+									if(l.AnalizarOper(tkn.get(2), tkn.get(1)))
+									{
+										l.codop=tkn.get(1);
+										l.etq=tkn.get(0);
+										l.oper=tkn.get(2);
+										l.linea=Integer.toString(contlinea);
+										vfinal.add(l);
+									}
+									banm=1;
 									break;
+								}
+							}
 						}
 						
 						if(banopr==1)
@@ -478,17 +578,12 @@ public class Principal extends JFrame {
 							break;
 						}
 						
-						if(banm==1)
-							vfinal.add(l);
-						else
-							Errores(6,0,"");
-							
+						if(banm==0)
+							Errores(6,0,tkn.get(1));
 					}
-					
 				}
 				else	
 					Errores(3,tkn.size(),"");
-				
 				
 				break;
 				
@@ -496,7 +591,6 @@ public class Principal extends JFrame {
 				Errores(3,opc,"");
 				break;
 		}
-		
 	}
 	
 	public void Errores(int opc,int pos,String cad)
@@ -504,6 +598,11 @@ public class Principal extends JFrame {
 		Error e=new Error();
 		switch(opc)
 		{
+		    case 3: 
+	     	    e.linea=Integer.toString(contlinea);
+	    	    e.error="Excede los 3 argumentos permitidos.";
+	    	    elista.add(e);
+	    	break;
 		    case 4: 
 	    	    e.linea="X";
 	    	    e.error="Falta la directiva END";
@@ -511,25 +610,29 @@ public class Principal extends JFrame {
 	    	break;
 		    case 5: 
 		    	e.linea=Integer.toString(contlinea);
-		    	e.error="No es un código de operación válido: "+cad;
+		    	e.error="Necesita tokens de tabulación/espacio para ser una línea válida: "+cad;
 		    	elista.add(e);
 		    	break;
 		    case 6: 
 		    	e.linea=Integer.toString(contlinea);
-		    	e.error="No coincide el CODOP: \" "+cad+" \" con el TABOP.";
+		    	e.error="No coincide el CODOP: "+cad+" con el TABOP.";
 		    	elista.add(e);
 		    	break;
 		    case 7: 
 		    	e.linea=Integer.toString(contlinea);
-		    	e.error="Este modo de direccionamiento: \" "+cad+" \" no permite operandos.";
+		    	e.error="Este modo de direccionamiento: "+cad+" no permite operandos.";
 		    	elista.add(e);
 		    	break;
 		    case 8: 
 		    	e.linea=Integer.toString(contlinea);
-		    	e.error="Este modo de direccionamiento: \" "+cad+" \" necesita operandos..";
+		    	e.error="Este modo de direccionamiento: "+cad+" necesita operandos.";
 		    	elista.add(e);
 		    	break;
-		    	
+		    case 9: 
+		    	e.linea=Integer.toString(contlinea);
+		    	e.error="Operando ORG necesita un operando.";
+		    	elista.add(e);
+		    	break;
 		}
 	}
 	
